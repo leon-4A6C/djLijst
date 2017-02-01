0d2286c02931a6f62d0591f6bed44272759e2acb
leon3110l <leon3110l@gmail.com>
2017-02-01 21:23:48 +0100

    update

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
 var json = ophalen("dj_lijsten.json");
 console.log(json);
-
-
-
-
-function ophalen(url) {
-  var request = new XMLHttpRequest();
-  request.open("GET", url, false);
-  var response = request.responseText;
-  request.send();
-  return JSON.parse(response);
-}
